1b7a1c9ec041e682ebb6ac2822df431012a1f27b
Ricky Aditya <rickyda79@gmail.com>
2019-09-26 16:08:34 +0700

    Build dist

---
+++ dist/precache-manifest.4ceba4d749cfb08f468eaeea05c67281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8e8134abb82df372f04",
+    "url": "/css/app.ba0e76ba.css"
+  },
+  {
+    "revision": "c8e8134abb82df372f04",
+    "url": "/js/app.6002ca9f.js"
+  },
+  {
+    "revision": "33a5dfd348c771420172",
+    "url": "/css/chunk-vendors.091d3a5f.css"
+  },
+  {
+    "revision": "33a5dfd348c771420172",
+    "url": "/js/chunk-vendors.31b9481c.js"
+  },
+  {
+    "revision": "3d93c7f514fbcfa85ec2b5164b9df0df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
