28994ab73460153bb23999b79747f2a58b4a7bca
Ricky Aditya <rickyda79@gmail.com>
2020-06-24 15:13:37 +0700

    Change remote URL

---
+++ dist/precache-manifest.2bed05c671c6b0c4e0c5acdbf1bc8536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d0f6343ca21617453c6",
+    "url": "/css/chunk-vendors.e998c55a.css"
+  },
+  {
+    "revision": "8d0f6343ca21617453c6",
+    "url": "/js/chunk-vendors.9514f273.js"
+  },
+  {
+    "revision": "1ad85cf4ae1a1ba37d17",
+    "url": "/css/index.5602d50d.css"
+  },
+  {
+    "revision": "1ad85cf4ae1a1ba37d17",
+    "url": "/js/index.9c6cb18a.js"
+  },
+  {
+    "revision": "d463ec2cc34973ae5a5e46f57d8cc5ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a26df9e0a0b77f620dcd6e5e9f213711.js"
+  "/precache-manifest.2bed05c671c6b0c4e0c5acdbf1bc8536.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "k-tracking"});
