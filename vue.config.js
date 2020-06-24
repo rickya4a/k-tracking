b4def409e3b9f4e0c9581f41bb4dfa3205e5e50d
Ricky Aditya <rickyda79@gmail.com>
2020-06-24 15:06:07 +0700

    Change URL

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,22 @@
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   productionSourceMap: false,
-  configureWebpack: {
-    plugins: [
-      new FaviconsWebpackPlugin('./src/assets/logo.png')
-    ]
+  filenameHashing: true,
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'K-Tracking App',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
   }
 }
